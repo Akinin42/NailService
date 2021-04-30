3c7ba36feff2eb34ac97e7a8dc874c8492db30e0
Igor Akinin <akinin32167@gmail.com>
2021-04-30 22:39:46 +0700

    finished CustomerDaoImpl

--- nailservice/src/main/java/nailservice/dao/impl/CustomerDaoImpl.java
+++ nailservice/src/main/java/nailservice/dao/impl/CustomerDaoImpl.java
@@ -1,90 +1,53 @@
-package nailservice.dao;
+package nailservice.dao.impl;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.List;
-import nailservice.models.Customer;
+import java.util.Optional;
+import nailservice.dao.Connector;
+import nailservice.dao.CustomerDao;
+import nailservice.entity.Customer;
+import nailservice.exceptions.DaoException;
 
-public class CustomerDao {
+public class CustomerDaoImpl extends AbstractCrudImpl<Customer> implements CustomerDao {
+    
+    private static final String SAVE_QUERY = "INSERT INTO customers (name, phone) VALUES(?,?);";
+    private static final String FIND_BY_ID_QUERY = "SELECT * FROM customers WHERE customer_id = ?;";
+    private static final String FIND_ALL_QUERY = "SELECT * FROM customers ORDER BY customer_id;";
+    private static final String FIND_ALL_PAGINATION_QUERY = "SELECT * FROM customers ORDER BY customer_id LIMIT ? OFFSET ?;";
+    private static final String DELETE_BY_ID_QUERY = "DELETE FROM customers WHERE customer_id = ?;";
+    private static final String FIND_BY_PHONE_QUERY = "SELECT * FROM customers WHERE phone = ?";
 
-    private DAOFactory factory = DAOFactory.getInstance();
-
-    public void insert(Customer customer) throws IllegalArgumentException, DAOException {
-        String sql = "INSERT INTO customers (name, phone) VALUES(?,?);";
-        if (getByPhone(customer.getPhone()) != null) {
-            throw new IllegalArgumentException("Customer is already created!");
-        }
-        Object[] values = { customer.getName(), customer.getPhone() };
-        try (Connection connection = factory.getConnection();
-                PreparedStatement statement = DAOUtil.prepareStatement(connection, sql, true, values);) {
-            int affectedRows = statement.executeUpdate();
-            if (affectedRows == 0) {
-                throw new DAOException("Creating customer failed, no rows affected.");
-            }
-            try (ResultSet generatedKeys = statement.getGeneratedKeys()) {
-                if (generatedKeys.next()) {
-                    customer.setId(generatedKeys.getInt(1));
-                } else {
-                    throw new DAOException("Creating customer failed, no generated key obtained.");
-                }
-            }
-        } catch (SQLException e) {
-            throw new DAOException(e);
-        }
+    public CustomerDaoImpl(Connector connector) {
+        super(connector, SAVE_QUERY, FIND_BY_ID_QUERY, FIND_ALL_QUERY, FIND_ALL_PAGINATION_QUERY, DELETE_BY_ID_QUERY);
     }
 
-    public Customer getByPhone(String phone) throws DAOException {
-        Customer customer = null;
-        String sql = "SELECT * FROM customers WHERE phone = ?";
-        try (Connection connection = factory.getConnection();
-                PreparedStatement statement = DAOUtil.prepareStatement(connection, sql, false, phone);
-                ResultSet resultSet = statement.executeQuery();) {
-            if (resultSet.next()) {
-                customer = init(resultSet);
-            }
-        } catch (SQLException e) {
-            throw new DAOException(e);
-        }
-        return customer;
-    }
-    public Customer getById(int id) throws DAOException {
-        Customer customer = null;
-        String sql = "SELECT * FROM customers WHERE customer_id = ?";
-        try (Connection connection = factory.getConnection();
-                PreparedStatement statement = DAOUtil.prepareStatement(connection, sql, false, id);
-                ResultSet resultSet = statement.executeQuery();) {
-            if (resultSet.next()) {
-                customer = init(resultSet);
-            }
-        } catch (SQLException e) {
-            throw new DAOException(e);
-        }
-        return customer;
+    public Optional<Customer> findByPhone(String phone) throws DaoException {              
+        try (Connection connection = connector.getConnection();
+                PreparedStatement statement = connection.prepareStatement(FIND_BY_PHONE_QUERY)) {
+            statement.setString(1, phone);
+            try (ResultSet resultSet = statement.executeQuery()) {
+                return resultSet.next() ? Optional.ofNullable(createEntityFromResultSet(resultSet)) : Optional.empty();
+            }        
+        } catch (SQLException | NullPointerException e) {
+            throw new DaoException("Can't return by phone " + phone, e);
+        }        
     }
 
-    public List<Customer> getAll() throws DAOException {
-        List<Customer> customers = new ArrayList<>();
-        String sql = "SELECT customer_id, name, phone FROM customers ORDER BY customer_id";
-        try (Connection connection = factory.getConnection();
-                PreparedStatement statement = connection.prepareStatement(sql);
-                ResultSet resultSet = statement.executeQuery();) {
-            while (resultSet.next()) {
-                customers.add(init(resultSet));
-            }
-        } catch (SQLException e) {
-            throw new DAOException(e);
-        }
-        return customers;
+    @Override
+    protected void insert(PreparedStatement statement, Customer customer) throws SQLException {
+        statement.setInt(1, customer.getId());
+        statement.setString(2, customer.getName());
+        statement.setString(3, customer.getPhone());        
     }
 
-    private Customer init(ResultSet resultSet) throws SQLException {
-        Customer customer = new Customer();
-        customer.setId(resultSet.getInt("customer_id"));
-        customer.setName(resultSet.getString("name"));
-        customer.setPhone(resultSet.getString("phone"));
-        return customer;
+    @Override
+    protected Customer createEntityFromResultSet(ResultSet resultSet) throws SQLException {
+        return Customer.builder()
+                .withId(resultSet.getInt("customer_id"))
+                .withName(resultSet.getString("name"))
+                .withPhone(resultSet.getString("phone"))
+                .build();
     }
 }
