6ddfce0aee2a226705180a75af067b3564010db9
Igor Akinin <akinin32167@gmail.com>
2021-04-30 23:47:29 +0700

    change OrderDao and implement it

--- nailservice/src/main/java/nailservice/dao/OrderDao.java
+++ nailservice/src/main/java/nailservice/dao/OrderDao.java
@@ -1,116 +1,18 @@
 package nailservice.dao;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.time.LocalDate;
 import java.time.LocalTime;
-import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
-import nailservice.models.Order;
+import nailservice.entity.Order;
 
-public class OrderDao {
+public interface OrderDao extends CrudDao<Order, Integer> {
+    
+    Optional<Order> findByTime(LocalDate date, LocalTime time);
+    
+    List<Order> findAllOfDay(LocalDate date);
+    
+    List<Order> findAllOfWeek(LocalDate startDate, LocalDate finishDate);
 
-    private DAOFactory factory = DAOFactory.getInstance();
-
-    public void insert(Order order) throws IllegalArgumentException, DAOException {
-        String sql = "INSERT INTO orders (date, time, customer_id, nailservice_id) VALUES(?,?,?,?);";
-        if (order.getId() != null) {
-            throw new IllegalArgumentException("NailService is already created, y can only update it!");
-        }
-        Object[] values = { order.getDate(), order.getTime(), order.getCustomer().getId(),
-                order.getNailService().getId() };
-        try (Connection connection = factory.getConnection();
-                PreparedStatement statement = DAOUtil.prepareStatement(connection, sql, true, values);) {
-            int affectedRows = statement.executeUpdate();
-            if (affectedRows == 0) {
-                throw new DAOException("Creating order failed, no rows affected.");
-            }
-            try (ResultSet generatedKeys = statement.getGeneratedKeys()) {
-                if (generatedKeys.next()) {
-                    order.setId(generatedKeys.getInt(1));
-                } else {
-                    throw new DAOException("Creating order failed, no generated key obtained.");
-                }
-            }
-        } catch (SQLException e) {
-            throw new DAOException(e);
-        }
-    }
-
-    public Order getByTime(LocalDate date, LocalTime time) throws DAOException {
-        Order order = null;
-        String sql = "SELECT * FROM orders WHERE date = ? AND time = ?";
-        Object[] values = { date, time };
-        try (Connection connection = factory.getConnection();
-                PreparedStatement statement = DAOUtil.prepareStatement(connection, sql, false, values);
-                ResultSet resultSet = statement.executeQuery();) {
-            if (resultSet.next()) {
-                order = init(resultSet);
-            }
-        } catch (SQLException e) {
-            throw new DAOException(e);
-        }
-        return order;
-    }
-
-    public List<Order> getOrdersOfDay(LocalDate date) throws DAOException {
-        List<Order> ordersOfDay = new ArrayList<>();
-        String sql = "SELECT order_id, date, time, customer_id, nailservice_id FROM orders WHERE date = ? ORDER BY time";
-        try (Connection connection = factory.getConnection();
-                PreparedStatement statement = DAOUtil.prepareStatement(connection, sql, true, date);
-                ResultSet resultSet = statement.executeQuery();) {
-            while (resultSet.next()) {
-                ordersOfDay.add(init(resultSet));
-            }
-        } catch (SQLException e) {
-            throw new DAOException(e);
-        }
-        return ordersOfDay;
-    }
-
-    public List<Order> getOrdersOfWeek(LocalDate startDate, LocalDate finishDate) throws DAOException {
-        List<Order> ordersOfDay = new ArrayList<>();
-        String sql = "SELECT order_id, date, time, customer_id, nailservice_id FROM orders WHERE date BETWEEN ? AND ? ORDER BY date, time";
-        Object[] values = { startDate, finishDate };
-        try (Connection connection = factory.getConnection();
-                PreparedStatement statement = DAOUtil.prepareStatement(connection, sql, true, values);
-                ResultSet resultSet = statement.executeQuery();) {
-            while (resultSet.next()) {
-                ordersOfDay.add(init(resultSet));
-            }
-        } catch (SQLException e) {
-            throw new DAOException(e);
-        }
-        return ordersOfDay;
-    }
-
-    public void delete(Order order) throws IllegalArgumentException, DAOException {
-        String sql = "DELETE FROM orders WHERE order_id = ?;";
-        try (Connection connection = factory.getConnection();
-                PreparedStatement statement = DAOUtil.prepareStatement(connection, sql, true, order.getId());) {
-            int affectedRows = statement.executeUpdate();
-            if (affectedRows == 0) {
-                throw new DAOException("Deleting order failed, no rows affected.");
-            } else {
-                order.setId(null);
-            }
-        } catch (SQLException e) {
-            throw new DAOException(e);
-        }
-    }
-
-    private Order init(ResultSet resultSet) throws SQLException {
-        CustomerDao customerDao = factory.getCustomerDao();
-        NailServiceDao serviceDao = factory.getNailServiceDao();
-        Order order = new Order();
-        order.setId(resultSet.getInt("order_id"));
-        order.setDate(resultSet.getObject("date", LocalDate.class));
-        order.setTime(resultSet.getObject("time", LocalTime.class));
-        order.setCustomer(customerDao.getById(resultSet.getInt("customer_id")));
-        order.setNailService(serviceDao.getById(resultSet.getInt("nailservice_id")));
-        return order;
-    }
 }
---
+++ nailservice/src/main/java/nailservice/dao/impl/OrderDaoImpl.java
@@ -0,0 +1,100 @@
+package nailservice.dao.impl;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import nailservice.dao.Connector;
+import nailservice.dao.CustomerDao;
+import nailservice.dao.NailServiceDao;
+import nailservice.dao.OrderDao;
+import nailservice.entity.Order;
+import nailservice.exceptions.DaoException;
+
+public class OrderDaoImpl extends AbstractCrudImpl<Order> implements OrderDao {
+
+    private static final String SAVE_QUERY = "INSERT INTO orders (date, time, customer_id, nailservice_id) VALUES(?,?,?,?);";
+    private static final String FIND_BY_ID_QUERY = "SELECT * FROM orders WHERE order_id = ?;";
+    private static final String FIND_ALL_QUERY = "SELECT * FROM orders ORDER BY order_id;";
+    private static final String FIND_ALL_PAGINATION_QUERY = "SELECT * FROM orders ORDER BY order_id LIMIT ? OFFSET ?;";
+    private static final String DELETE_BY_ID_QUERY = "DELETE FROM orders WHERE order_id = ?;";
+    private static final String FIND_BY_TIME_QUERY = "SELECT * FROM orders WHERE date = ? AND time = ?;";
+    private static final String FIND_ALL_BY_DAY_QUERY = "SELECT * FROM orders WHERE date = ? ORDER BY time;";
+    private static final String FIND_ALL_BY_WEEK_QUERY = "SELECT * FROM orders WHERE date BETWEEN ? AND ? ORDER BY date, time;";
+
+    public OrderDaoImpl(Connector connector) {
+        super(connector, SAVE_QUERY, FIND_BY_ID_QUERY, FIND_ALL_QUERY, FIND_ALL_PAGINATION_QUERY, DELETE_BY_ID_QUERY);
+    }
+
+    public Optional<Order> findByTime(LocalDate date, LocalTime time) throws DaoException {
+        try (Connection connection = connector.getConnection();
+                PreparedStatement statement = connection.prepareStatement(FIND_BY_TIME_QUERY)) {
+            statement.setObject(1, date);
+            statement.setObject(2, time);
+            try (ResultSet resultSet = statement.executeQuery()) {
+                return resultSet.next() ? Optional.ofNullable(createEntityFromResultSet(resultSet)) : Optional.empty();
+            }
+        } catch (SQLException | NullPointerException e) {
+            throw new DaoException("Can't return by time " + time + " at " + date, e);
+        }
+    }
+
+    public List<Order> findAllOfDay(LocalDate date) throws DaoException {
+        List<Order> ordersOfDay = new ArrayList<>();
+        try (Connection connection = connector.getConnection();
+                PreparedStatement statement = connection.prepareStatement(FIND_ALL_BY_DAY_QUERY)) {
+            statement.setObject(1, date);
+            try (ResultSet resultSet = statement.executeQuery()) {
+                while (resultSet.next()) {
+                    ordersOfDay.add(createEntityFromResultSet(resultSet));
+                }
+            }
+            return ordersOfDay;
+        } catch (SQLException | NullPointerException e) {
+            throw new DaoException("Can't return by day " + date, e);
+        }
+    }
+
+    public List<Order> findAllOfWeek(LocalDate startDate, LocalDate finishDate) throws DaoException {
+        List<Order> ordersOfDay = new ArrayList<>();
+        try (Connection connection = connector.getConnection();
+                PreparedStatement statement = connection.prepareStatement(FIND_ALL_BY_WEEK_QUERY)) {
+            statement.setObject(1, startDate);
+            statement.setObject(1, finishDate);
+            try (ResultSet resultSet = statement.executeQuery()) {
+                while (resultSet.next()) {
+                    ordersOfDay.add(createEntityFromResultSet(resultSet));
+                }
+            }
+            return ordersOfDay;
+        } catch (SQLException | NullPointerException e) {
+            throw new DaoException("Can't return from day " + startDate + " to " + finishDate, e);
+        }
+    }    
+
+    @Override
+    protected void insert(PreparedStatement statement, Order order) throws SQLException {
+        statement.setInt(1, order.getId());
+        statement.setObject(2, order.getDate());
+        statement.setObject(3, order.getTime());
+        statement.setObject(4, order.getCustomer());
+        statement.setObject(5, order.getNailService());
+    }
+
+    @Override
+    protected Order createEntityFromResultSet(ResultSet resultSet) throws SQLException {
+        CustomerDao customerDao = new CustomerDaoImpl(connector);
+        NailServiceDao nailServiceDao = new NailServiceDaoImpl(connector);
+        return Order.builder().withId(resultSet.getInt("order_id"))
+                .withDate(resultSet.getObject("date", LocalDate.class))
+                .withTime(resultSet.getObject("time", LocalTime.class))
+                .withCustomer(customerDao.findById(resultSet.getInt("customer_id")).get())
+                .withNailService(nailServiceDao.findById(resultSet.getInt("nailservice_id")).get())
+                .build();
+    }
+}
