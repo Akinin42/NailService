e90676f8c367d38ce35ec82f12fa32265ab9a630
Igor Akinin <akinin32167@gmail.com>
2021-05-01 15:44:03 +0700

    change Administrator class

--- nailservice/src/main/java/nailservice/domain/Administrator.java
+++ nailservice/src/main/java/nailservice/domain/Administrator.java
@@ -3,60 +3,66 @@ package nailservice.domain;
 import java.time.LocalDate;
 import java.time.LocalTime;
 import java.util.List;
+import java.util.Optional;
 import nailservice.dao.CustomerDao;
-import nailservice.dao.DAOFactory;
 import nailservice.dao.NailServiceDao;
 import nailservice.dao.OrderDao;
-import nailservice.models.Customer;
-import nailservice.models.NailService;
-import nailservice.models.Order;
+import nailservice.entity.Customer;
+import nailservice.entity.NailService;
+import nailservice.entity.Order;
 
 public class Administrator {
+    
+    private final OrderDao orderDao;
+    private final CustomerDao customerDao;
+    private final NailServiceDao nailServiceDao;
 
-    private DAOFactory factory = DAOFactory.getInstance();
-    private OrderDao orderDao = factory.getOrderDao();
+    public Administrator(OrderDao orderDao, CustomerDao customerDao, NailServiceDao nailServiceDao) {       
+        this.orderDao = orderDao;
+        this.customerDao = customerDao;
+        this.nailServiceDao = nailServiceDao;
+    }
 
     public List<Order> getWeekOrders(String inputDate) {
         LocalDate startDate = LocalDate.parse(inputDate);
-        LocalDate finishDate = startDate.plusDays(7);
-        return orderDao.getOrdersOfWeek(startDate, finishDate);
+        LocalDate finishDate = startDate.plusDays(7);        
+        return orderDao.findAllOfWeek(startDate, finishDate);
     }
 
     public void createOrder(String customerName, String customerPhone, String inputDate, String inputTime,
-            String nailService) {
-        CustomerDao customerDao = factory.getCustomerDao();
-        NailServiceDao serviceDao = factory.getNailServiceDao();
-        Customer customer = customerDao.getByPhone(customerPhone);
-        if (customer == null) {
+            String nailServiceName) {        
+        Customer customer = customerDao.findByPhone(customerPhone).get();
+        if (customer.equals(Optional.empty())) {
             customer = createCustomer(customerName, customerPhone);
         }
-        NailService service = serviceDao.getByName(nailService);
+        NailService nailService = nailServiceDao.findByName(nailServiceName).get();
         LocalDate date = LocalDate.parse(inputDate);
         LocalTime time = LocalTime.parse(inputTime);
-        if (orderDao.getByTime(date, time) != null) {
-            throw new IllegalArgumentException("Sorry you can't get order on this time!");
+        if (!orderDao.findByTime(date, time).equals(Optional.empty())) {
+            throw new IllegalArgumentException("Sorry you can't get order on this time!"); // not IllegalArg
         }
-        Order order = new Order();
-        order.setDate(date);
-        order.setTime(time);
-        order.setNailService(service);
-        order.setCustomer(customer);
-        orderDao.insert(order);
+        Order order = Order.builder()
+                .withDate(date)
+                .withTime(time)
+                .withNailService(nailService)
+                .withCustomer(customer)
+                .build();        
+        orderDao.save(order);
     }
 
-    public void deleteOrder(String inputDate, String inputTime) {
+    public void deleteOrder(String inputDate, String inputTime) {        
         LocalDate date = LocalDate.parse(inputDate);
         LocalTime time = LocalTime.parse(inputTime);
-        Order order = orderDao.getByTime(date, time);
-        orderDao.delete(order);
+        Order order = orderDao.findByTime(date, time).get();
+        orderDao.deleteById(order.getId());
     }
 
-    private Customer createCustomer(String customerName, String customerPhone) {
-        CustomerDao customerDao = factory.getCustomerDao();
-        Customer customer = new Customer();
-        customer.setName(customerName);
-        customer.setPhone(customerPhone);
-        customerDao.insert(customer);
-        return customerDao.getByPhone(customerPhone);
+    private Customer createCustomer(String customerName, String customerPhone) {       
+        Customer customer = Customer.builder()
+                .withName(customerName)
+                .withPhone(customerPhone)
+                .build();        
+        customerDao.save(customer);
+        return customerDao.findByPhone(customerPhone).get();
     }
 }
