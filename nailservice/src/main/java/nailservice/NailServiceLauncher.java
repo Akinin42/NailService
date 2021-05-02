bcbde1b24fcadf2eea2ce1e6740fb0ae2a72d1c0
Igor Akinin <akinin32167@gmail.com>
2021-05-02 13:19:22 +0700

    add Spring Context

--- nailservice/src/main/java/nailservice/NailServiceLauncher.java
+++ nailservice/src/main/java/nailservice/NailServiceLauncher.java
@@ -1,35 +1,23 @@
 package nailservice;
 
-import org.postgresql.ds.PGSimpleDataSource;
-
-import nailservice.dao.Connector;
-import nailservice.dao.CustomerDao;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import nailservice.context.ApplicationContextInjector;
 import nailservice.dao.NailServiceDao;
-import nailservice.dao.OrderDao;
 import nailservice.dao.ScriptExecutor;
-import nailservice.dao.impl.ConnectorPostgres;
-import nailservice.dao.impl.CustomerDaoImpl;
-import nailservice.dao.impl.NailServiceDaoImpl;
-import nailservice.dao.impl.OrderDaoImpl;
 import nailservice.domain.Administrator;
 import nailservice.entity.NailService;
-import nailservice.io.FileReader;
 
-public class NailServiceLauncher {
-    
-    private static final String PROPERTIES_FILE = "postgresdb";
+public class NailServiceLauncher {    
+
     private static final String SQL_SCRIPT_FILE = "schema.sql";
     
     public static void main(String[] args) {
-        PGSimpleDataSource dataSource = new PGSimpleDataSource();
-        Connector connector = new ConnectorPostgres(PROPERTIES_FILE, dataSource);
-        FileReader reader = new FileReader();
-        ScriptExecutor executor = new ScriptExecutor(connector, reader);
+        ApplicationContext context = new AnnotationConfigApplicationContext(ApplicationContextInjector.class);
+        ScriptExecutor executor = context.getBean(ScriptExecutor.class);
         executor.executeScript(SQL_SCRIPT_FILE);
-        OrderDao orderDao = new OrderDaoImpl(connector);
-        CustomerDao customerDao = new CustomerDaoImpl(connector);
-        NailServiceDao nailServiceDao = new NailServiceDaoImpl(connector);
-        Administrator administrator = new Administrator(orderDao, customerDao, nailServiceDao);
+        NailServiceDao nailServiceDao = context.getBean(NailServiceDao.class);
+        Administrator administrator = context.getBean(Administrator.class);
         NailService manicure = NailService.builder().withName("Manicure").withCost(800).withDuration(120).build();
         NailService pedicure = NailService.builder().withName("Pedicure").withCost(1000).withDuration(90).build();
         nailServiceDao.save(manicure);
